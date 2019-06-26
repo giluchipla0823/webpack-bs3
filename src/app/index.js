10b1c6ba8f0d262746f2e391579e302e63c150d6
Gino Chirinos <gino.chirinos@ticmash.com>
2019-06-26 16:11:29 +0200

    Configuración

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+// Jquery
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+// Bootstrap
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+
+require('font-awesome/css/font-awesome.css');
